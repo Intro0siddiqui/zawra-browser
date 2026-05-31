de6db4fdff92fcf3eacc2d0d22ae912c9df9026a
Intro0siddiqui <introsiddique@gmail.com>
2026-05-31 22:42:42 +0530

    fix: link libc on root_module of executable

--- dependencies/z-graphics/build.zig
+++ dependencies/z-graphics/build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
-    smoke_test.link_libc = true;
+    smoke_test.root_module.link_libc = true;
     if (target.result.os.tag == .linux) {
         smoke_test.linkSystemLibrary("vulkan");
     }

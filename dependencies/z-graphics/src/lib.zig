b861c0ef1f4122e5f269f0f17a063325513b150a
Intro0siddiqui <introsiddique@gmail.com>
2026-05-31 19:08:19 +0530

    fix: ignore unused params in CreateWindow

--- dependencies/z-graphics/src/lib.zig
+++ dependencies/z-graphics/src/lib.zig
@@ -2,4 +2,8 @@
 pub const ZawraGraphicsHandle = *anyopaque;
 
 pub export fn ZawraGraphics_Initialize() bool { return true; }
-pub export fn ZawraGraphics_CreateWindow(width: u32, height: u32) ?ZawraGraphicsHandle { return null; }
+pub export fn ZawraGraphics_CreateWindow(width: u32, height: u32) ?ZawraGraphicsHandle {
+    _ = width;
+    _ = height;
+    return null;
+}

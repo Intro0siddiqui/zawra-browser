cfcee886e12411873803f57b4582c9b9615600d1
Intro0siddiqui <introsiddique@gmail.com>
2026-05-31 19:36:04 +0530

    fix: use addStaticLibrary to fix Vulkan linking

--- dependencies/z-graphics/build.zig
+++ dependencies/z-graphics/build.zig
@@ -4,14 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addLibrary(.{
+    const lib = b.addStaticLibrary(.{
         .name = "z-graphics",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/lib.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-        .linkage = .static,
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
     });
     if (target.result.os.tag == .linux) {
         lib.linkSystemLibrary("vulkan");

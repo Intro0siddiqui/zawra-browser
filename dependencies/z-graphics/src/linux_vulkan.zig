def8239dd29645cff3fcd6c4ccba8951dbaab474
Intro0siddiqui <introsiddique@gmail.com>
2026-06-01 11:34:17 +0530

    feat: re-implement real vulkan instance creation

--- dependencies/z-graphics/src/linux_vulkan.zig
+++ dependencies/z-graphics/src/linux_vulkan.zig
@@ -1,6 +1,37 @@
 const std = @import("std");
 const builtin = @import("builtin");
+const c = if (builtin.os.tag == .linux) @cImport({
+    @cInclude("vulkan/vulkan.h");
+}) else struct {};
 
 pub fn initInstance() ?*anyopaque {
-    return null;
+    if (builtin.os.tag != .linux) return null;
+
+    var app_info = std.mem.zeroInit(c.VkApplicationInfo, .{
+        .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pNext = null,
+        .pApplicationName = "Zawra",
+        .applicationVersion = c.VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "Zawra",
+        .engineVersion = c.VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = c.VK_API_VERSION_1_0,
+    });
+
+    var create_info = std.mem.zeroInit(c.VkInstanceCreateInfo, .{
+        .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = null,
+        .flags = 0,
+        .pApplicationInfo = &app_info,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = null,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = null,
+    });
+
+    var instance: c.VkInstance = null;
+    if (c.vkCreateInstance(&create_info, null, &instance) != c.VK_SUCCESS) {
+        return null;
+    }
+
+    return @ptrCast(instance);
 }
